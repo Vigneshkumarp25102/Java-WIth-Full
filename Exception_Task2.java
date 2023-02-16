6b3f2b0f5d07c703b2b60f951347d045d1652b54
Vigneshkumarp25102 <113236424+Vigneshkumarp25102@users.noreply.github.com>
2023-02-16 23:22:17 +0530

    Add files via upload

---
+++ Exception_Task2.java
@@ -0,0 +1,76 @@
+import sun.security.util.Password;
+
+class My_UserException extends Exception{
+    public My_UserException(String userExceptiontThrow){
+        super(userExceptiontThrow);
+    }
+}
+class My_PasswordRequiredException extends Exception{
+    public My_PasswordRequiredException(String PasswordRequiredExceptionThrow){
+        super(PasswordRequiredExceptionThrow);
+    }
+}
+class My_PasswordWrongException extends Exception{
+    public My_PasswordWrongException(String PasswordWrongExceptionThrow){
+        super(PasswordWrongExceptionThrow);
+    }
+}
+class UserNamePassword{
+    final String username="vignesh";
+    final String password="Vignesh25#";
+    public boolean username(String givenUsername) throws My_UserException {
+        if(givenUsername=="" || givenUsername==null || givenUsername!=username){
+            throw new My_UserException("Please enter the valid username");
+        }else {
+            return true;
+        }
+    }
+    public boolean passwordValidation(String givenpassword) throws My_PasswordRequiredException {
+        String valid="^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        if(!(givenpassword.matches(valid))){
+            throw new My_PasswordRequiredException("Password must contains minimum 8 characters and any one Special character");
+        }
+        else {
+            return givenpassword.matches(valid);
+        }
+    }
+    public boolean password(String givenPassword) throws My_PasswordWrongException {
+        if(password!=givenPassword){
+            throw new My_PasswordWrongException("Password doesn't match");
+        }
+        else {
+            return password==givenPassword;
+        }
+    }
+}
+public class Exception_Task2 {
+
+    public static void main(String arg[]){
+
+        UserNamePassword user = new UserNamePassword();
+        String userGivenUsername ="vignesh";
+        String userGivenPassword="Vignesh25#";
+
+        try{
+                if(user.username(userGivenUsername)== true){
+                    try {
+                        if(user.passwordValidation(userGivenPassword)== true){
+                            try{
+                            if(user.password(userGivenPassword)==true){
+                                System.out.println("Login Successfully");
+                            }
+                            }catch (My_PasswordWrongException throwPasswordWrongException){
+                                System.out.println(throwPasswordWrongException.getMessage());
+                            }
+                        }
+                    }catch (My_PasswordRequiredException throwPasswordRequiredException){
+                        System.out.println(throwPasswordRequiredException.getMessage());
+                    }
+
+                }
+        }catch (My_UserException throwUserException){
+            System.out.println(throwUserException.getMessage());
+        }
+
+    }
+}
